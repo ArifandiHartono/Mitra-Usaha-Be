5de077fa874b403fbf3954f766820c80c55b67aa
ArifandiHartono <arifandi.hartono@gmail.com>
2021-12-16 22:14:38 +0700

    update

--- controllers/transaksi.js
+++ controllers/transaksi.js
@@ -33,7 +33,7 @@ class transaksiController {
             barangdata.save()
             if(barangdata.stok <= barangdata.minimal_stok)
             {
-              let createnotif = { isread: false, message: "Segera restock produk " + barangdata.nama }
+              let createnotif = { isread: false, message: barangdata.nama }
               await notif.create(createnotif)
               //ini notif create
             }
